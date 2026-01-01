ca3377d432623ebd234b99edff36a16793a72fc0
SharpIce <VupRbl@163.com>
2026-01-01 22:24:58 +0800

    Initial commit
    This is just a backup. All commits will be squashed into the main branch once the basic framework is completed.
    
    Signed-off-by: SharpIce <VupRbl@163.com>

---
+++ eslint.config.ts
@@ -0,0 +1,52 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import jsdoc from 'eslint-plugin-jsdoc';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+const config = defineConfig([
+	eslint.configs.recommended,
+	tseslint.configs.strictTypeChecked,
+	tseslint.configs.stylisticTypeChecked,
+	jsdoc.configs['flat/recommended-typescript-error'],
+	eslintPluginUnicorn.configs.recommended,
+	eslintConfigPrettier,
+	{
+		languageOptions: {
+			globals: globals.node,
+			parserOptions: {
+				sourceType: 'module',
+				ecmaVersion: 'latest',
+				tsconfigRootDir: import.meta.dirname,
+				projectService: {
+					allowDefaultProject: ['eslint.config.ts'],
+				},
+			},
+		},
+		rules: {
+			eqeqeq: 'error',
+			'unicorn/prevent-abbreviations': [
+				'error',
+				{
+					checkFilenames: false,
+				},
+			],
+			'jsdoc/tag-lines': [
+				'warn',
+				'any',
+				{
+					startLines: 1,
+					endLines: 0,
+				},
+			],
+		},
+	},
+	{
+		files: ['*.ts', '*.tsx'],
+		ignores: ['node_modules', 'dist', 'data'],
+	},
+]);
+
+export default config;
